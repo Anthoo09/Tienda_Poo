b6b51ba845f316418e3ae7989a9e931f4b5dad9f
Anthoo09 <av.quinonezv@uea.edu.ec>
2025-08-30 22:14:54 -0500

    Delete Curso_one directory

--- Curso_one/Notas.py
+++
@@ -1,7 +0,0 @@
-nota = int(input("Ingresa tu calificacion: "))
-if nota < 7:
-    print(f"SACASTE UN {nota}: REPROBASTE")
-elif nota >= 7 and nota < 9:
-    print(f"SACASTE UN {nota}: APROBASTE")
-else:
-    print(f"SACASTE UN {nota}: ¡EXCELENTE ESTUDIANTE!")
